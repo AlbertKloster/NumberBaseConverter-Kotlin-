959d2837e5038123c805cc156d051622a2f5d0da
AlbertKloster <albert.kloster@gmail.com>
2023-06-21 17:53:05 -0400

    Stage4

--- Converter.kt
+++ Converter.kt
@@ -1,43 +1,72 @@
 package converter
 
-import java.math.BigInteger
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+const val FLOATING_POINT_PRECISION = 5
+const val BIG_DECIMAL_PRECISION = 100
 
 class Converter {
-    fun decimalToBase(decimalNumber: String, base: String): String {
-        val decimal = decimalNumber.toBigInteger()
-        val stringBuilder = StringBuilder()
-
-        if (decimal == BigInteger.ZERO) return "0"
-
-        var num = decimal
-
-        while (num > BigInteger.ZERO) {
-            val remainder = (num % base.toBigInteger()).toInt()
-
-            if (remainder < 10) {
-                stringBuilder.append(remainder.toString())
-            } else {
-                val asciiChar = ('a'.code + remainder - 10).toChar()
-                stringBuilder.append(asciiChar)
-            }
-
-            num /= base.toBigInteger()
-        }
-
-        return stringBuilder.reverse().toString()
-    }
 
     fun baseToDecimal(number: String, base: String): String {
-        var decimal = BigInteger.ZERO
-        var power = BigInteger.ONE
+        val parts = number.split(".")
+        val wholePart = parts[0]
+        val fractionalPart = if (parts.size > 1) parts[1] else ""
 
-        for (i in number.length - 1 downTo 0) {
-            val digit = if (number[i].isDigit()) number[i].toString().toBigInteger() else (number[i].code - 'a'.code + 10).toBigInteger()
+        var decimal = BigDecimal.ZERO
+        var power = BigDecimal.ONE
+
+        for (i in wholePart.lastIndex downTo 0) {
+            val digit = if (wholePart[i].isDigit()) wholePart[i].toString().toBigDecimal() else (wholePart[i].code - 'a'.code + 10).toBigDecimal()
             decimal += digit * power
-            power *= base.toBigInteger()
+            power *= base.toBigDecimal()
+        }
+
+        if (fractionalPart.isNotEmpty()) {
+            power = BigDecimal.ONE.setScale(BIG_DECIMAL_PRECISION) / base.toBigDecimal()
+
+            for (i in fractionalPart.indices) {
+                val digit = if (fractionalPart[i].isDigit()) fractionalPart[i].toString().toBigDecimal() else (fractionalPart[i].code - 'a'.code + 10).toBigDecimal()
+                decimal += digit * power
+                power /= base.toBigDecimal()
+            }
         }
 
         return decimal.toString()
+    }
+
+    fun decimalToBase(decimalNumber: String, base: String): String {
+
+        val parts = decimalNumber.split('.')
+        val wholePart = parts[0]
+        val fractionalPart = parts.getOrElse(1) { "" }
+
+        val stringBuilder = StringBuilder()
+
+        var quotient = wholePart.toBigDecimal().abs().setScale(BIG_DECIMAL_PRECISION)
+        while (quotient >= BigDecimal.ONE) {
+            val remainder = (quotient % BigDecimal(base)).toInt()
+            stringBuilder.insert(0, if (remainder < 10) remainder else ('a' + remainder - 10).toString())
+            quotient /= BigDecimal(base)
+        }
+
+        if (fractionalPart.isEmpty()) {
+            return stringBuilder.toString()
+        }
+
+        stringBuilder.append('.')
+
+        var remainingFraction = BigDecimal("0.$fractionalPart")
+
+        repeat(FLOATING_POINT_PRECISION) {
+            remainingFraction *= base.toBigDecimal()
+            val digit = remainingFraction.setScale(0, RoundingMode.DOWN)
+            val digitInt = digit.toInt()
+            stringBuilder.append(if (digitInt < 10) digitInt.toString() else ('a' + digitInt - 10).toString())
+            remainingFraction -= digit
+        }
+
+        return stringBuilder.toString()
     }
 
 }
